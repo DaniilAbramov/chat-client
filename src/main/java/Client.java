5cddeca7d2d08a23bd942fb5f91e64d3f73c7f32
DaniilAbramov <dan7326@yandex.ru>
2020-07-29 10:55:04 +0300

    edits

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,13 +1,15 @@
 import lombok.SneakyThrows;
+import utils.MyResourceBundle;
+import utils.Props;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Locale;
 
 public class Client {
     private final static String IP = "localhost";
     private final static int PORT = 8080;
+    private final static MyResourceBundle RESOURCE_BUNDLE = new MyResourceBundle(
+            new Locale(Props.getValue("language"), Props.getValue("country")));
 
     @SneakyThrows
     public void start() {
@@ -29,21 +31,24 @@ public class Client {
 
     public void registrationOrAuthorization(MessageReceiver messageReceiver, MessageSender messageSender) {
         System.out.println("=============================");
-        System.out.println("Добро пожаловать в чат!");
+//        System.out.println("Добро пожаловать в чат!");
+        System.out.println(RESOURCE_BUNDLE.getValue("welcome"));
         System.out.println("=============================");
 
-        System.out.println("Нажмите:\n 1 для Авторизации: \n 2 для Регистрации: ");
+//        System.out.println("Нажмите:\n 1 для Авторизации: \n 2 для Регистрации: ");
+        System.out.println(RESOURCE_BUNDLE.getValue("authorizationOrRegistration"));
         int numFromConsole = Integer.parseInt(messageReceiver.readMessage().trim());
         if (numFromConsole == 1) {
-            String name = getString(messageReceiver, "Введите имя:").toUpperCase();
-            String password = getString(messageReceiver, "Введите пароль:");
+            String name = getString(messageReceiver, RESOURCE_BUNDLE.getValue("entryName")).toUpperCase();
+            String password = getString(messageReceiver, RESOURCE_BUNDLE.getValue("entryPassword"));
             messageSender.sendMessage("Authorization " + name + " " + password);
         } else if (numFromConsole == 2) {
-            String name = getString(messageReceiver, "Введите имя:").toUpperCase();
-            String password = getString(messageReceiver, "Введите пароль:");
+            String name = getString(messageReceiver, RESOURCE_BUNDLE.getValue("entryName")).toUpperCase();
+            String password = getString(messageReceiver, RESOURCE_BUNDLE.getValue("entryPassword"));
             messageSender.sendMessage("Registration " + name + " " + password);
         } else {
-            System.err.println("Введите 1 или 2");
+//            System.err.println("Введите 1 или 2");
+            System.err.println(RESOURCE_BUNDLE.getValue("press1or2"));
             registrationOrAuthorization(messageReceiver, messageSender);
         }
 
---
+++ src/main/java/utils/Props.java
@@ -0,0 +1,22 @@
+package utils;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class Props {
+
+    private final static Properties properties;
+
+    static {
+        properties = new Properties();
+        try {
+            properties.load(Props.class.getResourceAsStream("/application.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getValue(String key) {
+        return properties.getProperty(key);
+    }
+}
